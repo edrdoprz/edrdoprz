099b202aa7ae7dc1abc4c363eeb9ee0f9dd1f533
Eduardo Perez <edrdo.prz@gmail.com>
2015-06-24 23:20:13 -0400

    added octicons

--- Brocfile.js
+++ Brocfile.js
@@ -53,5 +53,11 @@ var app = new EmberApp({
 // modules that you would like to import into your application
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
+app.import('vendor/octicons.woff', {
+	destDir: 'assets'
+});
+app.import('vendor/In-Black-28px.png', {
+	destDir: 'assets'
+});
 
 module.exports = app.toTree();
